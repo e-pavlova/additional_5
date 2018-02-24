962d31271e90a2d4871738d827a0649b098d90b2
Ekaterina Pavlova <pavlova.ea.91@gmail.com>
2018-02-24 23:43:45 +0300

    Implement function for the sequence of brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig)
+{
+    let bracketsObj = {};
+
+    for (let i = 0; i < bracketsConfig.length; i ++)
+    {
+        for (let j = 0; j < bracketsConfig[i].length; j ++)
+        {
+            var brackAsKey = bracketsConfig[i][j];
+
+            if( bracketsObj[brackAsKey] !== undefined)
+            {
+                bracketsObj[brackAsKey] = {isThisTheSameSubArr: i, isTheSameBrack: true, name: brackAsKey};
+            }
+            else
+            {
+                bracketsObj[brackAsKey] = {isOpen: j === 0 ? true : false, isThisTheSameSubArr: i, name: brackAsKey};
+            }
+        }
+    }
+
+    let openBrackets = [];
+
+    for (let i = 0; i < str.length; i ++)
+    {
+        var currSymb = str[i];
+        var currSymbValue = bracketsObj[currSymb];
+        let isOpen = currSymbValue.isOpen || (currSymbValue.isTheSameBrack === true && openBrackets.some((x) => {return x.name === currSymbValue.name}) !== true);
+
+        if (isOpen === true)
+        {
+            openBrackets.push(currSymbValue);
+        }
+        else
+        {
+            if (openBrackets.length > 0)
+            {
+                let theLastArrElem = openBrackets[openBrackets.length-1];
+                if (currSymbValue.isThisTheSameSubArr === theLastArrElem.isThisTheSameSubArr)
+                {
+                    openBrackets.splice(openBrackets.length-1, 1);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    if (openBrackets.length !== 0)
+    {
+        return false;
+    }
+    return true;
 }
